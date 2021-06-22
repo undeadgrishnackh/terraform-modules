edfedfae24ed6e46a7e15779600a80dfc5ba4bd5
undeadgrishnackh <undeadgrishnackh@gmail.com>
2021-06-22 07:41:28 +0200

    fix: exposed laod_balancer ARN using the aws_lb.lb_arn output insted of terraform documentation

--- loadbalancer/output.tf
+++ loadbalancer/output.tf
@@ -1,5 +1,9 @@
 output "alb_arn" {
-  value = aws_lb.arn
+  # the definition of the variable is defined into the output.tf of the AWS provider
+  # https://github.com/terraform-aws-modules/terraform-aws-alb/blob/8f72fe772b4f9152064b68e0ce3fffcae9a3fe68/outputs.tf
+  # the tricky part consists into the LB as an array of LBs --> TEST: aws elbv2 describe-load-balancers
+  description = "The ID and ARN of the load balancer we created."
+  value       = aws_lb.lb_arn
 }
 
 output "alb_listener_arn" {
